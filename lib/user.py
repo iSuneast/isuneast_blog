cc5558fcf8a8790d28d6581653e5888cd56d1c41
iSuneast <isuneast@gmail.com>
2014-10-14 16:32:38 +0800

    new file:   lib/user.py 	new file:   lib/user.pyc 	modified:   main.py 	modified:   main.pyc 	modified:   static/main.css

---
+++ lib/user.py
@@ -0,0 +1,102 @@
+import re
+from google.appengine.ext import db
+from lib.utils import Handler, Secure
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    password_hash = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+    @classmethod
+    def by_name(cls, username):
+        username = username.lower()
+        return User.all().filter('username = ', username).get()
+
+    @classmethod
+    def register(cls, username, password):
+        username = username.lower()
+        password_hash = Secure.make_password_hash(username, password)
+        user = User(username = username, password_hash = password_hash)
+        user.put()
+        return user
+
+    @classmethod
+    def login(cls, username, password):
+        user = cls.by_name(username)
+        if not user:
+            print 'user not found'
+        else:
+            print 'user found: %s ' % user.username
+        if user and Secure.valid_password(username, password, user.password_hash):
+            return user
+
+
+class LogInPage(Handler):
+    def get(self):
+        username = self.request.cookies.get('username')
+        if username is None:
+            username = ''
+        self.render('login.html', username = username)
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        user = User.by_name(username)
+        error = ''
+        if not user:
+            error = 'Invalid Username'
+        elif not Secure.valid_password(username, password, user.password_hash):
+            error = 'Invalid Password'
+
+        if error == '':
+            self.login(user)
+        else:
+            self.render('login.html', username = username, error = error)
+
+
+class LogOutPage(Handler):
+    def get(self):
+        self.logout()
+
+
+class SignUpPage(Handler):
+    __USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    __PASS_RE = re.compile(r"^.{3,20}$")
+
+    @staticmethod
+    def valid_username(username):
+        return username and SignUpPage.__USER_RE.match(username)
+
+    @staticmethod
+    def valid_password(password):
+        return password and SignUpPage.__PASS_RE.match(password)
+
+    def get(self):
+        self.render('signup.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+
+        error = ''
+        if not SignUpPage.valid_username(username):
+            error = 'Invalid Username'
+        elif username.lower() != 'isuneast':
+            error = 'The registration is closed'
+        elif not SignUpPage.valid_password(password):
+            error = 'Invalid Password'
+        elif password != verify:
+            error = 'Password does NOT match'
+        elif User.by_name(username) is not None:
+            error = 'Username already exisit. ToT'
+
+        if error != '':
+            self.render('signup.html', username = username, error = error)
+        else:
+            self.signup(username, password)
+
+    def signup(self, username, password):
+        user = User.register(username, password)
+        self.login(user)
--- main.py
+++ main.py
@@ -15,108 +15,9 @@ import datetime
 from pytz.gae import pytz
 from google.appengine.ext import db
 from lib.utils import Handler, Secure
+from lib.user import User, LogInPage, LogOutPage, SignUpPage
 
 plt.switch_backend('Agg')
-
-class User(db.Model):
-    username = db.StringProperty(required = True)
-    password_hash = db.StringProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-
-    @classmethod
-    def by_name(cls, username):
-        username = username.lower()
-        return User.all().filter('username = ', username).get()
-
-    @classmethod
-    def register(cls, username, password):
-        username = username.lower()
-        password_hash = Secure.make_password_hash(username, password)
-        user = User(username = username, password_hash = password_hash)
-        user.put()
-        return user
-
-    @classmethod
-    def login(cls, username, password):
-        user = cls.by_name(username)
-        if not user:
-            print 'user not found'
-        else:
-            print 'user found: %s ' % user.username
-        if user and Secure.valid_password(username, password, user.password_hash):
-            return user
-
-
-class LogInPage(Handler):
-    def get(self):
-        username = self.request.cookies.get('username')
-        if username is None:
-            username = ''
-        self.render('login.html', username = username)
-
-    def post(self):
-        username = self.request.get('username')
-        password = self.request.get('password')
-
-        user = User.by_name(username)
-        error = ''
-        if not user:
-            error = 'Invalid Username'
-        elif not Secure.valid_password(username, password, user.password_hash):
-            error = 'Invalid Password'
-
-        if error == '':
-            self.login(user)
-        else:
-            self.render('login.html', username = username, error = error)
-
-
-class LogOutPage(Handler):
-    def get(self):
-        self.logout()
-
-
-class SignUpPage(Handler):
-    __USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-    __PASS_RE = re.compile(r"^.{3,20}$")
-
-    @staticmethod
-    def valid_username(username):
-        return username and SignUpPage.__USER_RE.match(username)
-
-    @staticmethod
-    def valid_password(password):
-        return password and SignUpPage.__PASS_RE.match(password)
-
-    def get(self):
-        self.render('signup.html')
-
-    def post(self):
-        username = self.request.get('username')
-        password = self.request.get('password')
-        verify = self.request.get('verify')
-
-        error = ''
-        if not SignUpPage.valid_username(username):
-            error = 'Invalid Username'
-        elif username.lower() != 'isuneast':
-            error = 'The registration is closed'
-        elif not SignUpPage.valid_password(password):
-            error = 'Invalid Password'
-        elif password != verify:
-            error = 'Password does NOT match'
-        elif User.by_name(username) is not None:
-            error = 'Username already exisit. ToT'
-
-        if error != '':
-            self.render('signup.html', username = username, error = error)
-        else:
-            self.signup(username, password)
-
-    def signup(self, username, password):
-        user = User.register(username, password)
-        self.login(user)
-
 
 class MainPage(Handler):
     def get(self):
@@ -168,7 +69,7 @@ class Activity(Handler):
         if reset:
             SportDiary.clear()
         url = 'http://i.cs.hku.hk/~wbtang/have_fun.txt'
-        activity = ['run', 'pull up', 'horizontal bar', 'sit up']
+        activity = ['run', 'pull up', 'push up', 'sit up'] # , 'horizontal bar'
 
         SportDiary.insert_diary(subject = 'activity', content = repr(activity))
         SportDiary.insert_diary(subject = 'since', content = '20140911')
@@ -258,6 +159,7 @@ class Activity(Handler):
 
     @classmethod
     def draw(cls, x, y, labels):
+#        plt.figure(figsize=(8, 8))
         for id in range(len(labels)):
             ax = plt.subplot(len(labels), 1, id+1)
             ax.plot(x, [y[i][id] for i in range(len(y))], 'ro-', markersize=12)
@@ -285,7 +187,7 @@ class Diary(db.Model):
     def get_content(self, raw=False):
         if raw:
             return eval(self.content)
-        return eval(self.content).replace('\n', '<br>')
+        return eval(self.content).replace('\\\r\n', '').replace('\n', '<br>')
 
     def get_created(self):
         return Secure.format_time(self.created, 8)
